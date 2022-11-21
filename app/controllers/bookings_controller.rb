34155730a51d5b29d1a59d59e50fa4adc8aaa79f
Danilo Guimaraes <guimaraes.louie@gmail.com>
2022-11-21 15:38:33 +0000

    Issues corrected and booking routes done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,6 +22,7 @@ class BookingsController < ApplicationController
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
+    redirect_to bookings_path, status: :see_other
   end
 
   private
--- app/controllers/pigs_controller.rb
+++ app/controllers/pigs_controller.rb
@@ -14,8 +14,11 @@ class PigsController < ApplicationController
 
   def create
     @pig = Pig.new(params[:pig])
-    @pig.save
-    rederict_to pig_path(@pig)
+    if @pig.save
+      redirect_to pig_path(@pig), status: :see_other
+    else
+      render new_pig_path, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -31,7 +34,7 @@ class PigsController < ApplicationController
   def destroy
     @pig = Pig.find(params[:id])
     @pig.destroy
-    redirect_to pig_pth, status: :see_other
+    redirect_to pigs_path, status: :see_other
   end
 
   private
