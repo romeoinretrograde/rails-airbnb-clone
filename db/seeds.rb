f7e8639d81f5e536a87cd594b76d11bc2276fba7
lorenz ploch <lorenzploch@hotmail.com>
2022-11-22 18:02:50 +0000

    images url added again

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,16 @@ User.destroy_all
 puts "creating owner of pigs"
 
 owner = User.create(email: "pig@master.com", password: "123456")
-pig_photos = [
-  
+pig_photos = ["https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139361/pig5_bif6qs.jpg",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139361/pig3_jlp9hg.jpg",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139361/pig4_lptps1.jpg",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139360/pig2_w6dkt9.jpg",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139360/pig1_yjma7v.jpg",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139359/pig17_hzm3qd.png",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139360/pig19_m2z0sc.jpg",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139360/pig16_rvyprf.png",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139360/pig12_aipaw9.png",
+  "https://res.cloudinary.com/dnk27mzjg/image/upload/v1669139360/pig18_yuwscs.jpg"
 ]
 
 puts "generating new pigs"
