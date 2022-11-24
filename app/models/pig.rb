2246dba38b2689c540994b9b77045c9c5a46d1de
lorenz ploch <lorenzploch@hotmail.com>
2022-11-24 17:28:54 +0000

    fixed minor details and added a map in pigs show

--- app/models/pig.rb
+++ app/models/pig.rb
@@ -4,6 +4,9 @@ class Pig < ApplicationRecord
 
   has_one_attached :photo
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   include PgSearch::Model
   pg_search_scope :global_search,
       against: [ :name, :details, :breed ],
