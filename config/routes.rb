66856de5e59fd83abdf1e16b855011ea4bb23307
lorenz ploch <lorenzploch@hotmail.com>
2022-11-25 12:40:55 +0000

    basic path done, added columns start_date and end_date

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home", as: "home"
-  resources :pigs
-  resources :bookings, only: [ :index, :new, :create, :destroy ]
+  resources :pigs do
+    resources :bookings, only: [ :index, :new, :create, :destroy ]
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
