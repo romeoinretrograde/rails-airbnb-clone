4e37939169e8c5ab563fa35bf36335b0a6f3b3e4
Danilo Guimaraes <guimaraes.louie@gmail.com>
2022-11-25 13:43:41 +0000

    part of the dashboard is done, waiting for the bookings to be functional so i can work on it here

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,9 @@ class PagesController < ApplicationController
   def home
     @pigs = Pig.all.sample(3)
   end
+
+  def dashboard
+    @pigs = Pig.select { |pig| pig.user_id == current_user.id }
+    @bookings = Booking.select { |booking| booking.user_id == current_user.id }
+  end
 end
